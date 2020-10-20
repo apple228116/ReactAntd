104e8248f66a292b01c69787fe20babe00655438
kimi <1941659296@qq.com>
2020-10-21 07:25:48 +0800

    redux

--- myantd/src/components/test.jsx
+++ myantd/src/components/test.jsx
@@ -1,38 +1,54 @@
 import React, { Component } from 'react'
 import {Button} from 'antd-mobile';
+import * as actions from '../redux/action'
+import PropTypes from 'prop-types'
 
 export default class test extends Component {
+
+  static propTypes = {
+    store: PropTypes.object.isRequired
+  }
+
+  increment = () => {
+    const number = Number(this.input.value)
+    this.props.store.dispatch(actions.increment(number))
+  }
+
+  decrement = () => {
+    const number = Number(this.input.value)
+    this.props.store.dispatch(actions.decrement(number))
+  }
 
   state = {
     num: 0
   }
 
-  culculateAdd = () => {
-    let tmpNum = this.state.num
-    tmpNum += Number(this.input.value) || tmpNum
-    this.setState({
-      num: tmpNum
-    })
-    this.input.value = 0
-  }
+  // culculateAdd = () => {
+  //   let tmpNum = this.state.num
+  //   tmpNum += Number(this.input.value) || tmpNum
+  //   this.setState({
+  //     num: tmpNum
+  //   })
+  //   this.input.value = 0
+  // }
 
-  culculateDecrease = () => {
-    let tmpNum = this.state.num
-    tmpNum -= Number(this.input.value) || tmpNum
-    this.setState({
-      num: tmpNum
-    })
-    this.input.value = 0
-  }
+  // culculateDecrease = () => {
+  //   let tmpNum = this.state.num
+  //   tmpNum -= Number(this.input.value) || tmpNum
+  //   this.setState({
+  //     num: tmpNum
+  //   })
+  //   this.input.value = 0
+  // }
 
   render() {
     return (
       <div>
-        <header>there is {this.state.num} caixukun!</header>
+        <header>there is {this.props.store.getState()} caixukun!</header>
         <div className='btnBox leftStart'>
           <input type='number' placeholder='鸡你太美！' ref={input=>this.input=input} />
-          <Button size='small' type='primary' onClick={this.culculateAdd}>添加</Button>
-          <Button size='small' type='dashed' onClick={this.culculateDecrease}>减少</Button>
+          <Button size='small' type='primary' onClick={this.increment} >添加</Button>
+          <Button size='small' type='dashed' onClick={this.decrement} >减少</Button>
           <Button size='small' type='primary'>延时添加</Button>
         </div>
       </div>
--- myantd/src/index.js
+++ myantd/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore} from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+import App from './components/test';
+import {counter} from './redux/reducers'
+
+const store = createStore(counter)
+
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+};
+
+render()
+
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
